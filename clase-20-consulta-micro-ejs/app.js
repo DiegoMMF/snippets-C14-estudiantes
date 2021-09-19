117718837e418c226127d6d2796f03be89bacebf
Diego M. Maldini Freyre <diegomaldinifreyre@gmail.com>
2021-09-19 20:36:23 -0300

    consulta EJS

---
+++ clase-20-consulta-micro-ejs/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+// línea antes de implementar carpeta partials
+app.get("/homeViejo", (req, res) => res.sendFile(__dirname + "/views/indexViejo.html"));
+
+app.set('view engine', 'ejs');
+
+app.get("/home", (req, res) => res.render(__dirname + "/views/index"))
+
+app.listen(3002, () => { console.log('Servidor arriba en http://localhost:3002');})
